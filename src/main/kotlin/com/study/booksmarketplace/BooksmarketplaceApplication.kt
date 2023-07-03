614b1f9f27ebc6626e996dede619f74db608f9c3
Thiago José da Silva <tjs.thiago@gmail.com>
2023-07-03 20:51:12 -0300

    initial commit

---
+++ src/main/kotlin/com/study/booksmarketplace/BooksmarketplaceApplication.kt
@@ -0,0 +1,11 @@
+package com.study.booksmarketplace
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BooksmarketplaceApplication
+
+fun main(args: Array<String>) {
+	runApplication<BooksmarketplaceApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/booksmarketplace/BooksmarketplaceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.booksmarketplace
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BooksmarketplaceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
