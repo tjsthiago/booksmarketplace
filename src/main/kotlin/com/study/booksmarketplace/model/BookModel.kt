cd52f80e3f9b88f68dd25cd7401cd892b9416c30
Thiago José da Silva <tjs.thiago@gmail.com>
2023-08-08 18:32:36 -0300

    blocking book status change

--- src/main/kotlin/com/study/booksmarketplace/model/BookModel.kt
+++ src/main/kotlin/com/study/booksmarketplace/model/BookModel.kt
@@ -5,7 +5,7 @@ import jakarta.persistence.*
 import java.math.BigDecimal
 
 @Entity(name = "book")
-data class BookModel (
+data class BookModel(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
@@ -16,11 +16,32 @@ data class BookModel (
     @Column
     var price: BigDecimal,
 
-    @Column
-    @Enumerated(EnumType.STRING)
-    var status: BookStatus? = null,
-
     @ManyToOne
     @JoinColumn(name = "customer_id")
     var customer: CustomerModel? = null
-)
+) {
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    var status: BookStatus? = null
+        set(value) {
+            if (field == BookStatus.CANCELLED || field == BookStatus.DELETED) {
+                throw Exception(
+                    "It is not allowed to change the status of a book with $field status"
+                )
+            }
+
+            field = value
+        }
+
+    constructor(
+        id: Long? = null,
+        name: String,
+        price: BigDecimal,
+        customer: CustomerModel? = null,
+        status: BookStatus?
+    ) : this(id, name, price, customer) {
+        this.status = status
+    }
+
+}
